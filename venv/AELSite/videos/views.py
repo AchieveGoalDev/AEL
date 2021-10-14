029a7cdc0ceca80aff216242316866aba673e40f
Justin Smith <justin.smith@achievegoal.jp>
2021-10-14 19:52:13 +0900

    begin filling out view for automatically populating videos in videos app

--- venv/AELSite/videos/views.py
+++ venv/AELSite/videos/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from videos import models
+
+course = models.Course.objects.get(id=1)
+videos = models.Category.objects.all()
+
+def getContext(videoQS):
+    context = {}
+    for video in videoQS:
+        if video.category not in context['categories']:
+            context['categories'] += video.category
+        context['videos'] += video
+
 @login_required
 def videos(request):
     return render(request, 'videos/videos-base.html') #Fill out dynamically
 
+@login_required
 def video(request):
     return render(request, 'videos/view-video.html') #Fill out dynamically 
